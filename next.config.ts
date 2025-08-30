c4ee5e781b47efbfbe3338dcf4ac730bda82ab2c
Eva Killenberg <>
2025-08-30 16:55:16 -0400

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,7 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  // Set basePath and assetPrefix for GitHub Pages
-  // Replace 'your-repo-name' with your actual repository name
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+  }
 }
 
 export default nextConfig;
