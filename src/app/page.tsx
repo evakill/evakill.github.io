c02f8d31c32c93b169282d9b7bc1c97525c3f034
Eva Killenberg <>
2025-09-03 22:56:46 -0400

    add 2025 site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col px-[25vw] py-10 w-full gap-6">
+    <div className="flex flex-col m-auto py-10 w-[50vw] min-w-[700px] gap-6">
       <h1 className="text-5xl">Digital HQ of <b>Eva Killenberg</b></h1>
       <p>Hi, my name is Eva! Here you will find links to various things I am working on and have worked on. Wanna chat? You can reach me at evakillenberg@gmail.com. Have a great day!</p>
       <h2 className="text-3xl">Enter Here</h2>
-      <p> 2025 Site Coming Soon :)</p>
+      <a href="https://2025.evakillenberg.com">
+        <div className="flex flex-col w-full hover:scale-101 transition-transform duration-600 ease-in-out border-4 border-black">
+          <div className="w-full bg-green-600 text-white text-left p-2 text-xl">
+            Check out my 2025 Portfolio
+          </div>
+          <Image
+            src="/v2025-header.png"
+            alt="My Website From 2025"
+            width={800}
+            height={432}
+            style={{ width: "80vw", height: "auto", display: "block", margin: "0 auto" }}
+            sizes="80vw"
+          />
+        </div>
+      </a>
       <h2 className="text-3xl">The Archive</h2>
       <a href="https://evakillenberg.com">
         <div className="flex flex-col w-full hover:scale-101 transition-transform duration-600 ease-in-out border-4 border-black">
