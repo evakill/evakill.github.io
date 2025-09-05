72f6fada051e93194cfd7f2107f0b5b8e2224254
Eva Killenberg <>
2025-09-05 09:38:38 -0400

    add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col m-auto py-10 w-[50vw] min-w-[700px] gap-6">
+    <div className="flex flex-col m-auto py-10 w-[50vw] min-w-[700px] gap-3">
       <h1 className="text-5xl">Digital HQ of <b>Eva Killenberg</b></h1>
+      <div className="flex gap-2">
+        <a className="underline" href="https://github.com/evakill">Github</a> &middot;
+        <a className="underline" href="https://www.linkedin.com/in/evakill/">LinkedIn</a>
+      </div>
       <p>Hi, my name is Eva! Here you will find links to various things I am working on and have worked on. Wanna chat? You can reach me at evakillenberg@gmail.com. Have a great day!</p>
-      <h2 className="text-3xl">Enter Here</h2>
+      <h2 className="text-3xl mt-3">Enter Here</h2>
       <a href="https://2025.evakillenberg.com">
         <div className="flex flex-col w-full hover:scale-101 transition-transform duration-600 ease-in-out border-4 border-black">
           <div className="w-full bg-green-600 text-white text-left p-2 text-xl">
@@ -21,7 +25,7 @@ export default function Home() {
           />
         </div>
       </a>
-      <h2 className="text-3xl">The Archive</h2>
+      <h2 className="text-3xl mt-3">The Archive</h2>
       <a href="http://evakillenberg.herokuapp.com">
         <div className="flex flex-col w-full hover:scale-101 transition-transform duration-600 ease-in-out border-4 border-black">
           <div className="w-full bg-evayellow text-white text-left p-2 text-xl">
